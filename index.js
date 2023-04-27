7b7e1bde0f07c01f9b3159d5326ea5dfabe80ec4
Kevin-Zapata <kevin19981921@gmail.com>
2023-04-26 23:58:21 -0500

    Estructura de carpetas, dependecias e iniciando servidor express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.send('Hola')
+});
+
+app.listen(PORT,() => { console.log('Server listening on port '+ PORT ) });
